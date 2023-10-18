ff0345655ecc829d3d92a92b21cf7f7988c09879
Roula Almoarawi <roula.almoarawi@iths.se>
2023-10-18 10:16:27 +0200

    Add project files.

---
+++ WPF Demo1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Demo1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> listOfPersons = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            listOfPersons.Add(new Person { FirstName = "Tarek", LastName = "Gustavson" });
+            listOfPersons.Add(new Person { FirstName = "Anton", LastName = "NotHere" });
+            listOfPersons.Add(new Person { FirstName = "Mosa", LastName = "Eld" });
+
+            ComboBox.ItemsSource = listOfPersons;
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {FirstNameText.Text} to our program");
+        }
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string FullName
+            {
+                get
+                {
+                    return $"{FirstName}  {LastName}";
+                }
+
+            }
+
+        }
+
+    }
+}
+
